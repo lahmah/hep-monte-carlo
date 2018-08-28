correct weight calculation in ImportanceMC; reject zero weight events; combine qcd densities into a single class

src/hepmc/core/densities/qcd.py
@@ -6,74 +6,16 @@ import pkg_resources
 import numpy as np
 from itertools import combinations
 
-
-CARD0 = pkg_resources.resource_filename('hepmc', 'data/ee_qq.dat')
-CARD1 = pkg_resources.resource_filename('hepmc', 'data/ee_qq_1g.dat')
-CARD2 = pkg_resources.resource_filename('hepmc', 'data/ee_qq_2g.dat')
-
-
-# e+ e- -> q qbar
-class ee_qq(Density):
-    def __init__(self, E_CM):
-        ndim = 8
+# e+ e- -> q qbar + n gluons
+class ee_qq_ng(Density):
+    def __init__(self, n_gluons, E_CM, pT_min, angle_min):
         self.conversion = 0.389379*1e9  # convert to picobarn
-        self.nfinal = 2  # number of final state particles
+        self.nfinal = 2+n_gluons  # number of final state particles
+        ndim = 4 * self.nfinal
 
         super().__init__(ndim, False)
 
-        self.E_CM = E_CM
-
-        self.Generator = Sherpa.Sherpa()
-        self.Generator.InitializeTheRun(3,
-                                        [''.encode('ascii'),
-                                         ('RUNDATA=' + CARD0).encode('ascii'),
-                                         'INIT_ONLY=2'.encode('ascii'),
-                                         'OUTPUT=0'.encode('ascii')])
-        self.Process = Sherpa.MEProcess(self.Generator)
-
-        # Incoming flavors must be added first!
-        self.Process.AddInFlav(11)
-        self.Process.AddInFlav(-11)
-        self.Process.AddOutFlav(1)
-        self.Process.AddOutFlav(-1)
-        self.Process.Initialize()
-
-        self.Process.SetMomentum(0, E_CM/2., 0., 0., E_CM/2.)
-        self.Process.SetMomentum(1, E_CM/2., 0., 0., -E_CM/2.)
-
-    # The first momentum is xs[0:4]
-    # The second momentum is xs[4:8]
-    def pdf(self, xs):
-        xs = interpret_array(xs, self.ndim)
-
-        ndim = xs.shape[1]
-
-        if ndim != self.ndim:
-            raise RuntimeWarning("Mismatching dimensions.")
-
-        p1 = xs[:, 0:4]
-        p2 = xs[:, 4:8]
-
-        sample_size = len(xs)
-        me = np.empty(sample_size)
-        for i in range(sample_size):
-            self.Process.SetMomentum(2, p1[i, 0], p1[i, 1], p1[i, 2], p1[i, 3])
-            self.Process.SetMomentum(3, p2[i, 0], p2[i, 1], p2[i, 2], p2[i, 3])
-            me[i] = self.Process.CSMatrixElement()
-
-        xs = (2. * np.pi) ** (4.-3. * self.nfinal) / (2. * self.E_CM ** 2) * me
-        return self.conversion * xs
-
-
-# e+ e- -> q qbar + gluon
-class ee_qq_1g(Density):
-    def __init__(self, E_CM, pT_min, angle_min):
-        ndim = 4 * 3
-        self.conversion = 0.389379*1e9  # convert to picobarn
-        self.nfinal = 3  # number of final state particles
-
-        super().__init__(ndim, False)
-
+        self.n_gluons = n_gluons
         self.E_CM = E_CM
         self.pT_min = pT_min
         self.angle_min = angle_min
@@ -81,7 +23,7 @@ class ee_qq_1g(Density):
         self.Generator = Sherpa.Sherpa()
         self.Generator.InitializeTheRun(4,
                                         [''.encode('ascii'),
-                                         ('RUNDATA=' + CARD1).encode('ascii'),
+                                         ('RUNDATA=' + path_to_runcard(n_gluons)).encode('ascii'),
                                          'INIT_ONLY=2'.encode('ascii'),
                                          'OUTPUT=0'.encode('ascii')])
         self.Process = Sherpa.MEProcess(self.Generator)
@@ -91,7 +33,8 @@ class ee_qq_1g(Density):
         self.Process.AddInFlav(-11)
         self.Process.AddOutFlav(1)
         self.Process.AddOutFlav(-1)
-        self.Process.AddOutFlav(21)
+        for _ in range(n_gluons):
+            self.Process.AddOutFlav(21)
         self.Process.Initialize()
 
         self.Process.SetMomentum(0, E_CM/2., 0., 0., E_CM/2.)
@@ -107,21 +50,20 @@ class ee_qq_1g(Density):
         if ndim != self.ndim:
             raise RuntimeWarning("Mismatching dimensions.")
 
-        p1 = xs[:, 0:4]
-        p2 = xs[:, 4:8]
-        p3 = xs[:, 8:12]
-
         sample_size = len(xs)
         me = np.empty(sample_size)
 
         cross_section = np.empty(xs.shape[0])
         for i in range(sample_size):
-            momenta = [p1[i], p2[i], p3[i]]
+            momenta = []
+            for j in range(self.nfinal):
+                momenta.append(xs[i, 4*j:4*j+4])
+
             # apply cuts
-            if all([self.pT(p)>self.pT_min for p in momenta]) and all([self.angle(p1, p2)>self.angle_min for p1, p2 in combinations(momenta, 2)]):
-                self.Process.SetMomentum(2, p1[i, 0], p1[i, 1], p1[i, 2], p1[i, 3])
-                self.Process.SetMomentum(3, p2[i, 0], p2[i, 1], p2[i, 2], p2[i, 3])
-                self.Process.SetMomentum(4, p3[i, 0], p3[i, 1], p3[i, 2], p3[i, 3])
+            if all([pT(p)>self.pT_min for p in momenta]) and all([angle(p1, p2)>self.angle_min for p1, p2 in combinations(momenta, 2)]):
+                for j, momentum in enumerate(momenta):
+                    self.Process.SetMomentum(j+2, momentum[0], momentum[1], momentum[2], momentum[3])
+
                 me[i] = self.Process.CSMatrixElement()
             else:
                 me[i] = 0.
@@ -130,88 +72,14 @@ class ee_qq_1g(Density):
 
         return self.conversion * cross_section
 
-    # Calculate transverse momentum of 4-vector
-    def pT(self, p):
-        return np.sqrt(p[1]*p[1]+p[2]*p[2])
+def path_to_runcard(n_gluons):
+    return pkg_resources.resource_filename('hepmc', 'data/ee_qq_' + str(n_gluons) + 'g.dat')
 
-    # Calculate angle between two 4-vectors
-    def angle(self, p, q):
-        cos_angle = (p[1:].dot(q[1:]))/(p[0]*q[0])
-        return np.arccos(cos_angle)
+# Calculate transverse momentum of 4-vector
+def pT(p):
+    return np.sqrt(p[1]*p[1]+p[2]*p[2])
 
-# e+ e- -> q qbar g g
-class ee_qq_2g(Density):
-    def __init__(self, E_CM, pT_min, angle_min):
-        ndim = 4 * 4
-        self.conversion = 0.389379*1e9  # convert to picobarn
-        self.nfinal = 4  # number of final state particles
-
-        super().__init__(ndim, False)
-
-        self.E_CM = E_CM
-        self.pT_min = pT_min
-        self.angle_min = angle_min
-
-        self.Generator = Sherpa.Sherpa()
-        self.Generator.InitializeTheRun(4,
-                                        [''.encode('ascii'),
-                                         ('RUNDATA=' + CARD2).encode('ascii'),
-                                         'INIT_ONLY=2'.encode('ascii'),
-                                         'OUTPUT=0'.encode('ascii')])
-        self.Process = Sherpa.MEProcess(self.Generator)
-
-        # Incoming flavors must be added first!
-        self.Process.AddInFlav(11)
-        self.Process.AddInFlav(-11)
-        self.Process.AddOutFlav(1)
-        self.Process.AddOutFlav(-1)
-        self.Process.AddOutFlav(21)
-        self.Process.AddOutFlav(21)
-        self.Process.Initialize()
-
-        self.Process.SetMomentum(0, E_CM/2., 0., 0., E_CM/2.)
-        self.Process.SetMomentum(1, E_CM/2., 0., 0., -E_CM/2.)
-
-    # The first momentum is xs[0:4]
-    # The second momentum is xs[4:8], ...
-    def pdf(self, xs):
-        xs = interpret_array(xs, self.ndim)
-
-        ndim = xs.shape[1]
-
-        if ndim != self.ndim:
-            raise RuntimeWarning("Mismatching dimensions.")
-
-        p1 = xs[:, 0:4]
-        p2 = xs[:, 4:8]
-        p3 = xs[:, 8:12]
-        p4 = xs[:, 12:16]
-
-        sample_size = len(xs)
-        me = np.empty(sample_size)
-
-        cross_section = np.empty(xs.shape[0])
-        for i in range(sample_size):
-            momenta = [p1[i], p2[i], p3[i], p4[i]]
-            # apply cuts
-            if all([self.pT(p)>self.pT_min for p in momenta]) and all([self.angle(p1, p2)>self.angle_min for p1, p2 in combinations(momenta, 2)]):
-                self.Process.SetMomentum(2, p1[i, 0], p1[i, 1], p1[i, 2], p1[i, 3])
-                self.Process.SetMomentum(3, p2[i, 0], p2[i, 1], p2[i, 2], p2[i, 3])
-                self.Process.SetMomentum(4, p3[i, 0], p3[i, 1], p3[i, 2], p3[i, 3])
-                self.Process.SetMomentum(5, p4[i, 0], p4[i, 1], p4[i, 2], p4[i, 3])
-                me[i] = self.Process.CSMatrixElement()
-            else:
-                me[i] = 0.
-
-            cross_section = (2. * np.pi) ** (4.-3. * self.nfinal) / (2. * self.E_CM ** 2) * me
-
-        return self.conversion * cross_section
-
-    # Calculate transverse momentum of 4-vector
-    def pT(self, p):
-        return np.sqrt(p[1]*p[1]+p[2]*p[2])
-
-    # Calculate angle between two 4-vectors
-    def angle(self, p, q):
-        cos_angle = (p[1:].dot(q[1:]))/(p[0]*q[0])
-        return np.arccos(cos_angle)
+# Calculate angle between two 4-vectors
+def angle(p, q):
+    cos_angle = (p[1:].dot(q[1:]))/(p[0]*q[0])
+    return np.arccos(cos_angle)

src/hepmc/core/integration/importance.py
@@ -39,15 +39,28 @@ class ImportanceMC(object):
         :param eval_count: Total number of function evaluations.
         :return: Tuple (integral_estimate, error_estimate).
         """
-        xs = self.dist.rvs(eval_count)
-        ys = fn(*xs.transpose())
-        weights = self.dist.pdf(xs)
+        xs = np.empty((eval_count, self.ndim))
+        ys = np.empty(eval_count)
+
+        trials = 0
+        for i in range(eval_count):
+            y = 0.
+            while y == 0.:
+                trials += 1
+                x = self.dist.rvs(1)
+                y = fn(x)
+            
+            xs[i] = x
+            ys[i] = y
+
+        importance_pdf = self.dist.pdf(xs)
+        weights = ys/importance_pdf
         sample = IntegrationSample(data=xs, function_values=ys, weights=weights)
 
         # integral estimate
-        sample.integral = np.mean(ys / weights)
+        sample.integral = eval_count/trials * np.mean(weights)
         # variance of the weighted function samples
-        sample.integral_err = np.sqrt(np.var(ys / weights) / eval_count)
+        sample.integral_err = np.sqrt(np.var(weights) * eval_count/trials**2)
         return sample
 
 