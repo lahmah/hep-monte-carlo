d209afa41df2e53c90357d18afe61665f07363f1
tjansse2 <timo.janssen@phys.uni-goettingen.de>
2018-08-28 15:33:03 +0200

    add cuts to ee_qq_1g

--- src/hepmc/core/densities/qcd.py
+++ src/hepmc/core/densities/qcd.py
@@ -4,6 +4,7 @@ from ..density import Density
 from ..util import interpret_array
 import pkg_resources
 import numpy as np
+from itertools import combinations
 
 
 CARD0 = pkg_resources.resource_filename('hepmc', 'data/ee_qq.dat')
@@ -65,7 +66,7 @@ class ee_qq(Density):
 
 # e+ e- -> q qbar + gluon
 class ee_qq_1g(Density):
-    def __init__(self, E_CM):
+    def __init__(self, E_CM, pT_min, angle_min):
         ndim = 4 * 3
         self.conversion = 0.389379*1e9  # convert to picobarn
         self.nfinal = 3  # number of final state particles
@@ -73,6 +74,8 @@ class ee_qq_1g(Density):
         super().__init__(ndim, False)
 
         self.E_CM = E_CM
+        self.pT_min = pT_min
+        self.angle_min = angle_min
 
         self.Generator = Sherpa.Sherpa()
         self.Generator.InitializeTheRun(4,
@@ -109,11 +112,28 @@ class ee_qq_1g(Density):
 
         sample_size = len(xs)
         me = np.empty(sample_size)
-        for i in range(sample_size):
-            self.Process.SetMomentum(2, p1[i, 0], p1[i, 1], p1[i, 2], p1[i, 3])
-            self.Process.SetMomentum(3, p2[i, 0], p2[i, 1], p2[i, 2], p2[i, 3])
-            self.Process.SetMomentum(4, p3[i, 0], p3[i, 1], p3[i, 2], p3[i, 3])
-            me[i] = self.Process.CSMatrixElement()
 
-        xs = (2. * np.pi) ** (4.-3. * self.nfinal) / (2. * self.E_CM ** 2) * me
-        return self.conversion * xs
+        cross_section = np.empty(xs.shape[0])
+        for i in range(sample_size):
+            momenta = [p1[i], p2[i], p3[i]]
+            # apply cuts
+            if all([self.pT(p)>self.pT_min for p in momenta]) and all([self.angle(p1, p2)>self.angle_min for p1, p2 in combinations(momenta, 2)]):
+                self.Process.SetMomentum(2, p1[i, 0], p1[i, 1], p1[i, 2], p1[i, 3])
+                self.Process.SetMomentum(3, p2[i, 0], p2[i, 1], p2[i, 2], p2[i, 3])
+                self.Process.SetMomentum(4, p3[i, 0], p3[i, 1], p3[i, 2], p3[i, 3])
+                me[i] = self.Process.CSMatrixElement()
+            else:
+                me[i] = 0.
+
+            cross_section = (2. * np.pi) ** (4.-3. * self.nfinal) / (2. * self.E_CM ** 2) * me
+
+        return self.conversion * cross_section
+
+    # Calculate transverse momentum of 4-vector
+    def pT(self, p):
+        return np.sqrt(p[1]*p[1]+p[2]*p[2])
+
+    # Calculate angle between two 4-vectors
+    def angle(self, p, q):
+        cos_angle = (p[1:].dot(q[1:]))/(p[0]*q[0])
+        return np.arccos(cos_angle)
