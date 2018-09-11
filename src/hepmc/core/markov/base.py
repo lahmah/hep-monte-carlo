4c2731ab999d8372685248db7355f767e8b0cad8
tjansse2 <timo.janssen@phys.uni-goettingen.de>
2018-09-11 12:38:50 +0200

    Allow for weights in mixed markov updates.

--- src/hepmc/core/markov/base.py
+++ src/hepmc/core/markov/base.py
@@ -196,25 +196,127 @@ class MixingMarkovUpdate(MarkovUpdate):
             state = update.init_state(init)
             update.init_adapt(state)
 
-    def next_state(self, state, iteration):
-        index = np.random.choice(self.updates_count, p=self.weights)
-        update = self.updates[index]
-
+    def next_state(self, state, iteration, update_index):
+        update = self.updates[update_index]
         try:
-            state = self.in_maps[index](state).flatten()
+            state = self.in_maps[update_index](state).flatten()
         except KeyError:
             pass
 
-        if self.masks[index] is None:
+        if self.masks[update_index] is None:
             state = update.init_state(state)
             next_state = update.next_state(state, iteration)
         else:
-            mask = self.masks[index]
+            mask = self.masks[update_index]
             state = np.copy(state)
             state[mask] = update.next_state(state[mask], iteration)
             next_state = state
 
         try:
-            return self.out_maps[index](next_state).flatten()
+            return self.out_maps[update_index](next_state).flatten()
         except KeyError:
             return next_state
+
+    def sample(self, sample_size, initial, out_mask=None, n_batches=20):
+        """ Generate a sample of given size.
+
+        :param sample_size: Number of samples to generate.
+        :param initial: Initial value of the Markov chain. Internally
+            converted to numpy array.
+        :param out_mask: Slice object, return only this slice of the output
+            chain (useful if sampler uses artificial variables).
+        :return: Numpy array with shape (sample_size, self.ndim).
+        """
+        # initialize sampling
+        state = self.init_state(np.atleast_1d(initial))
+        if len(state) != self.ndim:
+            raise ValueError('initial must have dimension ' + str(self.ndim))
+        self.init_adapt(state)  # initial adaptation
+
+        batch_length = int(sample_size/n_batches)
+
+        sample = MarkovSample()
+
+        tags = dict()
+        tagged = dict()
+
+        # produce a new initial state from a known update 
+        index = np.random.choice(self.updates_count, p=self.weights)
+        state = self.next_state(state, 0, index)
+
+        chain = np.empty((sample_size, self.ndim))
+        weights = np.empty(sample_size)
+        weight_owner = np.empty(sample_size, dtype=np.uint8)
+        chain[0] = state
+        try:
+            weights[0] = state.weight
+        except AttributeError:
+            # if the update is weightless, we set the weight to 1 and normalize it later
+            weights[0] = 1
+        weight_owner[0] = index
+
+        batch_accept = deque(maxlen=batch_length)
+        current_seq = 1 # current sequence length
+        max_seq = 1 # maximal sequence length
+        skip = 1
+        for i in range(1, sample_size):
+            index = np.random.choice(self.updates_count, p=self.weights)
+            state = self.next_state(state, i, index)
+            if not np.array_equal(state, chain[i - 1]):
+                batch_accept.append(1)
+                if current_seq > max_seq:
+                    max_seq = current_seq
+                current_seq = 1
+                sample.accepted += 1
+            else:
+                batch_accept.append(0)
+                current_seq += 1
+
+            chain[i] = state
+            try:
+                weights[i] = state.weight
+            except AttributeError:
+                # if the update is weightless, we set the weight to 1 and normalize it later
+                weights[i] = 1
+            weight_owner[i] = index
+
+            try:
+                try:
+                    tags[state.tag_parser].append(state.tag)
+                    tagged[state.tag_parser].append(i)
+                except KeyError:
+                    tags[state.tag_parser] = []
+                    tagged[state.tag_parser] = []
+            except AttributeError:
+                pass
+
+            if i % skip == 0:
+                if i >= batch_length:
+                    accept_rate = sum(batch_accept)/batch_length
+                else:
+                    accept_rate = sum(batch_accept)/i
+                if i == 1:
+                    print("Event 1\t(batch acceptance rate: %f)" % (accept_rate))
+                else:
+                    print("Event %i\t(batch acceptance rate: %f)\tmax sequence length: %i" % (i, accept_rate, max(current_seq, max_seq)))
+                if is_power_of_ten(i):
+                    skip *= 10
+
+        if out_mask is not None:
+            chain = chain[:, out_mask]
+
+        for parser in tagged:
+            chain[tagged[parser]] = parser(chain[tagged[parser]], tags[parser])
+
+        # normalize weights
+        for i in range(self.updates_count):
+            is_owned = weight_owner == i
+            mask = np.where(is_owned)
+            count = np.sum(is_owned)
+            sum_weights = np.sum(weights[mask])
+            weights[mask] = weights[mask] / sum_weights * (count/sample_size)
+
+        sample.data = chain
+        sample.weights = weights
+        sample.target = self.target
+        return sample
