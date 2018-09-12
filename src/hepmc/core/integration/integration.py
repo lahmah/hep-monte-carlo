5f199efe9e09f4764825e14b2720fe7edafca3a3
tjansse2 <timo.janssen@phys.uni-goettingen.de>
2018-09-12 15:22:29 +0200

    Rewrite integration module.
    - Remove IntegrationSample class. It is superseded by the more general
    Sample class.
    - Simplify PlainMC class and add documentation.

--- src/hepmc/core/integration/integration.py
+++ src/hepmc/core/integration/integration.py
@@ -1,18 +1,8 @@
 import numpy as np
+from typing import Tuple
 
 from ..sampling import Sample
-
-
-class IntegrationSample(Sample):
-
-    def __init__(self, **kwargs):
-        self.function_values = None
-
-        # computed by the integration methods
-        self.integral = None
-        self.integral_err = None
-
-        super().__init__(**kwargs)
+from ..density import Density
 
 
 class PlainMC(object):
@@ -21,30 +11,35 @@ class PlainMC(object):
     Approximate the integral as the mean of the integrand over a randomly
     selected sample (uniform probability distribution over the unit hypercube).
     """
-    def __init__(self, ndim=1, name="MC Plain"):
+    def __init__(self, ndim: int = 1, name: str = "MC Plain") -> None:
         self.method_name = name
         self.ndim = ndim
 
-    def __call__(self, fn, eval_count):
+    def __call__(self, target: Density, eval_count: int) -> Tuple[float, float]:
         """ Compute Monte Carlo estimate of ndim-dimensional integral of fn.
 
         The integration volume is the ndim-dimensional unit cube [0,1]^ndim.
 
-        :param fn: A function accepting self.ndim numpy arrays,
-            returning an array of the same length with the function values.
-        :param eval_count: Total number of function evaluations used to
+        Parameters
+        ----------
+        target
+            The target density.
+        eval_count
+            Total number of function evaluations used to
             approximate the integral.
-        :return: Tuple (integral_estimate, error_estimate) where
+
+        Returns
+        -------
+        Tuple[float, float]
+            Tuple (integral_estimate, error_estimate) where
             the error_estimate is based on the unbiased sample variance
             of the function, computed on the same sample as the integral.
             According to the central limit theorem, error_estimate approximates
             the standard deviation of the statistical (normal) distribution
             of the integral estimates.
         """
-        sample = IntegrationSample()
-        sample.data = np.random.random((eval_count, self.ndim))
-        sample.function_values = fn(*sample.data.transpose())
-        sample.integral = np.mean(sample.function_values)
-        err = np.sqrt(np.var(sample.function_values) / eval_count)
-        sample.integral_err = err
-        return sample
+        data = np.random.random((eval_count, self.ndim))
+        function_values = target.pdf(data)
+        integral = np.mean(function_values)
+        err = np.sqrt(np.var(function_values) / eval_count)
+        return integral, err
